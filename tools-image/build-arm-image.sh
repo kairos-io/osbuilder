eb6171b08c9da61ee1d02cb8e1f33810a1d8c0bd
Itxaka <itxaka.garcia@spectrocloud.com>
2023-07-17 10:19:32 +0200

    Revert "Really make it gpt"
    This reverts commit a923f2e5585d5fe11a9a03434062daa6503b7334.

--- tools-image/build-arm-image.sh
+++ tools-image/build-arm-image.sh
@@ -358,8 +358,7 @@ sgdisk -n 3:0:+$(( ${recovery_size} + ${oem_size} ))M -c 3:lvm -t 3:8e00 ${outpu
 fi
 sgdisk -n 4:0:+64M -c 4:persistent -t 4:8300 ${output_image}
 
-# Make the disk GPT
-sgdisk -g ${output_image}
+sgdisk -m 1:2:3:4 ${output_image}
 
 # Prepare the image and copy over the files
 
@@ -468,7 +467,7 @@ sync
 sleep 5
 sync
 
-kpartx -dvg $DRIVE || true
+kpartx -dv $DRIVE || true
 
 umount $DRIVE || true
 
