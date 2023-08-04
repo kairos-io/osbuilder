22050853ae8590179fd0e189421986cc7971bd6c
Dimitris Karakasilis <dimitris@karakasilis.me>
2023-08-04 18:14:58 +0300

    WIP
    Signed-off-by: Dimitris Karakasilis <dimitris@karakasilis.me>

--- controllers/osartifact_controller_test.go
+++ controllers/osartifact_controller_test.go
@@ -1,12 +1,37 @@
 package controllers
 
 import (
+	"context"
+
+	osbuilder "github.com/kairos-io/osbuilder/api/v1alpha2"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
 )
 
-var _ = Describe("", func() {
-	It("test", func() {
-		Expect(1).To(Equal(2))
+var _ = Describe("OSArtifactReconciler", func() {
+	var r *OSArtifactReconciler
+	var artifact *osbuilder.OSArtifact
+
+	BeforeEach(func() {
+		r = &OSArtifactReconciler{}
+		artifact = &osbuilder.OSArtifact{}
+
+		mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
+			Scheme: runtime.NewScheme(),
+		})
+		Expect(err).ToNot(HaveOccurred())
+
+		err = (r).SetupWithManager(mgr)
+		Expect(err).ToNot(HaveOccurred())
+	})
+
+	Describe("CreateConfigMap", func() {
+		It("test", func() {
+			ctx := context.Background()
+			err := r.CreateConfigMap(ctx, artifact)
+			Expect(err).ToNot(HaveOccurred())
+		})
 	})
 })
