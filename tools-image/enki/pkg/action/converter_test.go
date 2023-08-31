e5987cfbc3219c57a16387ce45b0597bb6707bf3
Dimitris Karakasilis <dimitris@karakasilis.me>
2023-08-31 15:33:33 +0300

    WIP
    Signed-off-by: Dimitris Karakasilis <dimitris@karakasilis.me>

---
+++ tools-image/enki/pkg/action/converter_test.go
@@ -0,0 +1,11 @@
+package action_test
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+	//. "github.com/onsi/gomega"
+)
+
+var _ = Describe("BuildISOAction", func() {
+	It("adds the framework bits", func() {
+	})
+})
